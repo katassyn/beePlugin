706e23ccaa1dff6131d36e415b3684acf07561ae
katassyn <mastu2115@gmail.com>
2025-08-22 14:46:10 +0200

    1st

---
+++ src/main/java/org/maks/beesPlugin/BeesPlugin.java
@@ -0,0 +1,17 @@
+package org.maks.beesPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BeesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
